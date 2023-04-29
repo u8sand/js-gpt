7200d4435cc1672581d270af5d88a8e76941e454
Daniel Clarke <u8sand@gmail.com>
2023-04-29 15:26:32 -0400

    Add publishing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  distDir: 'out/js-gpt',
+  assetPrefix: '/js-gpt',
 }
 
 module.exports = nextConfig
