38cc3420c06813db8186703211286fcf84c82929
Daniel Clarke <u8sand@gmail.com>
2023-04-29 16:22:33 -0400

    Updates

--- _next/static/lwu9axCgPK18jV3ZB1_ro/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/243-7b5822726d27c629.js","static/chunks/pages/index-f1b601551958dedd.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/x67VC6GxqQwKjj1VP3oNP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/243-7b5822726d27c629.js","static/chunks/pages/index-862de40dc52abecd.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
